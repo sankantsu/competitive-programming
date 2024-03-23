6055fa0222d3e713db1c29ebbd21f34b8fb9f19d
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-03-23 15:15:53 +0900

    update template.cc

--- template/template.cc
+++ template/template.cc
@@ -2,9 +2,10 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <tuple>
 #include <cassert>
 
-#define rep(i,n) for (long long i = 0; i < static_cast<long long>((n)); i++)
+#define rep(i,n) for (long i = 0; i < static_cast<long>((n)); i++)
 
 using namespace std;
 
