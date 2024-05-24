6745c53afe09eaef97e7268d65a1cbc1c09edf37
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-05-24 10:06:35 +0900

    chore: fix check.sh not to use `cargo run`

--- ahc/ahc033/check.sh
+++ ahc/ahc033/check.sh
@@ -4,13 +4,17 @@ in_dir="in"
 out_dir="out"
 log_dir="log"
 
+cargo build --release
+
+mkdir -p $out_dir
+mkdir -p $log_dir
+
 for i in `seq 0 999`; do
   echo "running $i..."
   digit=`printf "%04d" $i`
   in="${in_dir}/$digit.txt"
   out="${out_dir}/$digit.txt"
   log="${log_dir}/$digit.txt"
-  cargo run <$in >$out 2>$log &
-  sleep 0.05  # prevent failing lock acqusition
+  ./target/release/ahc033 <$in >$out 2>$log &
 done
 wait
