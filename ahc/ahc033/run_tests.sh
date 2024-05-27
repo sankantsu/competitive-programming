f1bfb73438360cf6b6d789e6d3b9acc9204f6409
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-05-27 15:39:13 +0900

    chore: update test runner script

--- ahc/ahc033/run_tests.sh
+++ ahc/ahc033/run_tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+n_testcase=$1
+
+if [[ -z $n_testcase ]]; then
+  echo "usage: $0 <n-testcase>"
+  exit 1
+fi
+
 in_dir="in"
 out_dir="out"
 log_dir="log"
@@ -9,12 +16,19 @@ cargo build --release
 mkdir -p $out_dir
 mkdir -p $log_dir
 
-for i in `seq 0 999`; do
-  echo "running $i..."
+n_batch=50
+
+for ((i=0; i<n_testcase; i++)); do
   digit=`printf "%04d" $i`
   in="${in_dir}/$digit.txt"
   out="${out_dir}/$digit.txt"
   log="${log_dir}/$digit.txt"
   ./target/release/ahc033 <$in >$out 2>$log &
+  if (((i+1)%n_batch == 0)); then
+    echo "Waiting jobs until $i th..."
+    wait
+  fi
 done
+
+echo "Waiting jobs until $i th..."
 wait
