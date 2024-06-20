1a24f76ac58ccb595222fea4d5e35ce47bb2adc3
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-06-20 18:04:58 +0900

    ahc034: implemented basic mcf strategy

---
+++ ahc/ahc034/src/bin/mcf.rs
@@ -0,0 +1,209 @@
+use ac_library::mincostflow::MinCostFlowGraph;
+
+#[derive(Clone, Debug)]
+struct Input {
+    n: usize,
+    h: Vec<Vec<i64>>,
+}
+
+#[derive(Clone, Debug, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Direction {
+    fn as_char(&self) -> char {
+        match self {
+            Self::Up => 'U',
+            Self::Right => 'R',
+            Self::Down => 'D',
+            Self::Left => 'L',
+        }
+    }
+}
+
+#[derive(Clone, Debug, Copy)]
+enum Action {
+    Move(Direction),
+    Load(i64),
+}
+
+impl Action {
+    fn as_str(&self) -> String {
+        match self {
+            Self::Move(dir) => dir.as_char().to_string(),
+            Self::Load(val) => {
+                if *val >= 0 {
+                    format!("+{val}")
+                } else {
+                    format!("{val}")
+                }
+            }
+        }
+    }
+}
+
+struct Solution {
+    actions: Vec<Action>,
+}
+
+impl Solution {
+    fn print(&self) {
+        for act in &self.actions {
+            let s = act.as_str();
+            println!("{s}");
+        }
+    }
+}
+
+fn input() -> Input {
+    proconio::input! {
+        n: usize,
+        h: [[i64; n]; n],
+    }
+    Input { n, h }
+}
+
+#[derive(Clone, Debug)]
+struct Route {
+    route: Vec<(usize, usize)>,
+}
+
+impl Route {
+    fn new(n: usize) -> Self {
+        let mut route = vec![];
+        for i in 0..n {
+            for j in 0..n {
+                if i % 2  == 0 {
+                    route.push((i, j));
+                } else {
+                    route.push((i, n - 1 - j));
+                }
+            }
+        }
+        for j in 0..n {
+            for i in 0..n {
+                if j % 2  == 0 {
+                    route.push((n - 1 - i, j));
+                } else {
+                    route.push((i, j));
+                }
+            }
+        }
+        Route { route }
+    }
+    fn len(&self) -> usize {
+        self.route.len()
+    }
+    fn nth(&self, i: usize) -> (usize, usize) {
+        self.route[i]
+    }
+    fn get_move(&self, i: usize) -> Option<Direction> {
+        let (si, sj) = self.nth(i);
+        let (ti, tj) = self.nth(i + 1);
+        let di = ti as i64 - si as i64;
+        let dj = tj as i64 - sj as i64;
+        if di == 0 && dj == 0 {
+            None
+        } else if di == -1 && dj == 0 {
+            Some(Direction::Up)
+        } else if di == 1 && dj == 0 {
+            Some(Direction::Down)
+        } else if di == 0 && dj == 1 {
+            Some(Direction::Right)
+        } else if di == 0 && dj == -1 {
+            Some(Direction::Left)
+        } else {
+            panic!("Invalid move");
+        }
+    }
+}
+
+fn solve(input: &Input) -> Solution {
+    let n = input.n;
+    let h = &input.h;
+
+    let route = Route::new(n);
+    let m = route.len();
+    // Vertices
+    // 0 .. n*n: correspond to each square
+    // n*m .. n*n + m: correspond to route
+    // n*n + m: source
+    // n*n + m + 1: sink
+    let mut graph = MinCostFlowGraph::new(n*n + m + 2);
+    // Edges
+    // source -> square: amount of soil to carry out (cap=h, cost=0)
+    let source = n*n + m;
+    for k in 0..n*n {
+        let i = k/n;
+        let j = k%n;
+        if h[i][j] > 0 {
+            graph.add_edge(source, k, h[i][j], 0);
+        }
+    }
+    // square -> sink: amount of soil to carry in (cap=-h, cost=0)
+    let sink = n*n + m + 1;
+    for k in 0..n*n {
+        let i = k/n;
+        let j = k%n;
+        if h[i][j] < 0 {
+            graph.add_edge(k, sink, -h[i][j], 0);
+        }
+    }
+    // route(i) -> route(i + 1): dump can carry soils along this flow (cap=inf, cost=1)
+    let inf = 1000000;
+    for k in 0..(m - 1) {
+        graph.add_edge(n*n + k, n*n + k + 1, inf, 1);
+    }
+    // square -> route(i): load soils (cap=inf, cost=1)
+    // route(i) -> square: unload soils (cap=inf, cost=1)
+    for k in 0..m {
+        let (i, j) = route.nth(k);
+        graph.add_edge(n*i + j, n*n + k, inf, 1);
+        graph.add_edge(n*n + k, n*i + j, inf, 1);
+    }
+    let (flow, cost) = graph.flow(source, sink, inf);
+    dbg!(flow, cost);
+
+    let edges = graph.edges();
+    let n_edge = edges.len();
+    let mut load_seq = vec![];
+    for k in 0..m {
+        let base = n_edge - 2*m + 2*k;
+        let load = edges[base].flow;
+        let unload = edges[base + 1].flow;
+        if load > 0 {
+            load_seq.push(load);
+        } else if unload > 0 {
+            load_seq.push(-unload);
+        } else {
+            load_seq.push(0);
+        }
+    }
+
+    let mut actions = vec![];
+    for k in 0..(m - 1) {
+        let load = load_seq[k];
+        if load != 0 {
+            actions.push(Action::Load(load));
+        }
+        let mv = route.get_move(k);
+        if mv.is_some() {
+            actions.push(Action::Move(mv.unwrap()));
+        }
+    }
+    let load = load_seq[m - 1];
+    if load != 0 {
+        actions.push(Action::Load(load));
+    }
+    Solution { actions }
+}
+
+fn main() {
+    let inpt = input();
+    let sol = solve(&inpt);
+    sol.print();
+}
